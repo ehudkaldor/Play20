4d29bb864a0c2df832a01c78c21d3eabf5e00d89
Ehud Kaldor <ehud.kaldor@gmail.com>
2012-05-03 00:20:35 -0700

    changed Evolution's 1.sql so that User will reflect parameters in class changed login message in login.scala.html to use 'admin@white-bears.org' with 'secret' if not configured yet commented sample projects and tasks creation in Global.scala

--- samples/scala/zentasks/conf/evolutions/default/1.sql
+++ samples/scala/zentasks/conf/evolutions/default/1.sql
@@ -4,8 +4,10 @@
 
 create table user (
   email                     varchar(255) not null primary key,
-  name                      varchar(255) not null,
-  password                  varchar(255) not null
+  password                  varchar(255) not null,
+  firstName                 varchar(255),
+  lastName                  varchar(255),
+  isAuthenticated	    boolean
 );
 
 create table project (
