0607721af7b5035275b9645509bd23ab824d2efd
Ehud Kaldor <ehud.kaldor@gmail.com>
2012-05-06 22:48:58 -0700

    changed gatling dep from 1.0 to 0.0.1

--- samples/scala/zentasks/project/Build.scala
+++ samples/scala/zentasks/project/Build.scala
@@ -11,13 +11,25 @@ object ApplicationBuild extends Build {
   /* Cloudbees plublic snapshot REPO */
   val cloudbeesRepo = "Cloudbees public snapshot" at "https://repository-andy-petrella.forge.cloudbees.com/snapshot"
 
-  val appDependencies = Seq(
-    "be.nextlab" %% "neo4j-rest-play-plugin" % "0.0.1-SNAPSHOT"
-  )
+  /* LOCAL MAVEN REPO */
+  val localMavenRepo = "Local Maven Repository" at file(Path.userHome.absolutePath+"/.m2/repository").toURI.toURL.toString
 
-//  val main = PlayProject(appName, appVersion, mainLang = SCALA)
+  val appDependencies = Seq(
+    "be.nextlab" %% "neo4j-rest-play-plugin" % "0.0.1-SNAPSHOT",
+
+
+    "org.specs2" %% "specs2" % "1.8.2" % "test" withSources,
+    "be.nextlab" %% "gatling-play2-plugin" % "0.0.1-SNAPSHOT" % "test"
+  )
+  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+  def customLessEntryPoints(base: File): PathFinder = (
+      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+      (base / "app" / "assets" / "stylesheets" * "*.less")
+    )
+
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    resolvers ++= Seq(cloudbeesRepo)
+    resolvers ++= Seq(cloudbeesRepo, localMavenRepo),
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
   )
 
 }
