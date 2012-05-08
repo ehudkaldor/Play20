538f863ff69733e2bea443d1f40488f81184b5b9
Ehud Kaldor <ehud.kaldor@gmail.com>
2012-05-07 23:44:49 -0700

    removed neo4j dep (too bad, i cannot get all deps of it resolved)

--- samples/scala/zentasks/project/Build.scala
+++ samples/scala/zentasks/project/Build.scala
@@ -15,22 +15,22 @@ object ApplicationBuild extends Build {
   val localMavenRepo = "Local Maven Repository" at file(Path.userHome.absolutePath+"/.ivy2/cache").toURI.toURL.toString
 
   val appDependencies = Seq(
-    "be.nextlab" %% "neo4j-rest-play-plugin" % "0.0.1-SNAPSHOT",
-
-
-    "org.specs2" %% "specs2" % "1.8.2" % "test" withSources,
-    "be.nextlab" %% "gatling-play2-plugin" % "0.0.1-SNAPSHOT" % "test"
+//    "be.nextlab" %% "neo4j-rest-play-plugin" % "0.0.1-SNAPSHOT",
+//
+//
+//    "org.specs2" %% "specs2" % "1.8.2" % "test" withSources,
+//    "be.nextlab" %% "gatling-play2-plugin" % "0.0.1-SNAPSHOT" % "test"
   )
-  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
-  def customLessEntryPoints(base: File): PathFinder = (
-      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
-      (base / "app" / "assets" / "stylesheets" * "*.less")
-    )
-
+//  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+//  def customLessEntryPoints(base: File): PathFinder = (
+//      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+//      (base / "app" / "assets" / "stylesheets" * "*.less")
+//    )
+//
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    resolvers ++= Seq(cloudbeesRepo, localMavenRepo),
-    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
+//    resolvers ++= Seq(/*cloudbeesRepo,*/ localMavenRepo),
+//    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
   )
-
+//
 }
             
