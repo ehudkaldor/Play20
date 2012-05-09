891e45e5c68ad783fa965a3c2d14cc35943608d6
Ehud Kaldor <ehud.kaldor@gmail.com>
2012-05-09 00:22:32 -0700

    added 2 default role, created on startup: "superuser" and "monitor"

--- samples/scala/zentasks/app/Global.scala
+++ samples/scala/zentasks/app/Global.scala
@@ -21,37 +21,11 @@ object InitialData {
   
   def insert() = {
     
+    Role.create(Role("superuser"))
+    Role.create(Role("monitor"))
+    
     if(User.findAll.isEmpty) {
-      User.create(User("admin@white-bears.org", "secret", isActivated=true))
-//      Seq(
-//        User("admin@white-bears.org", "secret")
-//      ).foreach(User.create)
-      
-//      Seq(
-//        Project(Id(1), "Play framework", "Play 2.0") -> Seq("guillaume@sample.com", "maxime@sample.com", "sadek@sample.com", "erwan@sample.com"),
-//        Project(Id(2), "Play framework", "Play 1.2.4") -> Seq("guillaume@sample.com", "erwan@sample.com"),
-//        Project(Id(3), "Play framework", "Website") -> Seq("guillaume@sample.com", "maxime@sample.com"),
-//        Project(Id(4), "Zenexity", "Secret project") -> Seq("guillaume@sample.com", "maxime@sample.com", "sadek@sample.com", "erwan@sample.com"),
-//        Project(Id(5), "Zenexity", "Playmate") -> Seq("maxime@sample.com"),
-//        Project(Id(6), "Personal", "Things to do") -> Seq("guillaume@sample.com"),
-//        Project(Id(7), "Zenexity", "Play samples") -> Seq("guillaume@sample.com", "maxime@sample.com"),
-//        Project(Id(8), "Personal", "Private") -> Seq("maxime@sample.com"),
-//        Project(Id(9), "Personal", "Private") -> Seq("guillaume@sample.com"),
-//        Project(Id(10), "Personal", "Private") -> Seq("erwan@sample.com"),
-//        Project(Id(11), "Personal", "Private") -> Seq("sadek@sample.com")
-//      ).foreach {
-//        case (project,members) => Project.create(project, members)
-//      }
-      
-//      Seq(
-//        Task(NotAssigned, "Todo", 1, "Fix the documentation", false, None, Some("guillaume@sample.com")),
-//        Task(NotAssigned, "Urgent", 1, "Prepare the beta release", false, Some(date("2011-11-15")), None),
-//        Task(NotAssigned, "Todo", 9, "Buy some milk", false, None, None),
-//        Task(NotAssigned, "Todo", 2, "Check 1.2.4-RC2", false, Some(date("2011-11-18")), Some("guillaume@sample.com")),
-//        Task(NotAssigned, "Todo", 7, "Finish zentask integration", true, Some(date("2011-11-15")), Some("maxime@sample.com")),
-//        Task(NotAssigned, "Todo", 4, "Release the secret project", false, Some(date("2012-01-01")), Some("sadek@sample.com"))
-//      ).foreach(Task.create)
-      
+      User.create(User("admin@white-bears.org", "secret", isActivated=true))     
     }
     
   }
