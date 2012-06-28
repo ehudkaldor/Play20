f84da956821e3b1cb39ac7d543815510598d0f6b
Ehud Kaldor <ehud.kaldor@gmail.com>
2012-06-27 23:57:41 -0700

    finished converting task to neo4j added Logger.debug

--- zentask/app/models/projects/Task.scala
+++ zentask/app/models/projects/Task.scala
@@ -1,36 +1,60 @@
 package models.projects
 
-import java.util.{Date}
-import play.api.db._
-import play.api.Play.current
-import org.neo4j.scala.{RestTypedTraverser, RestGraphDatabaseServiceProvider, Neo4jWrapper}
+import org.joda.time.DateTime
+import org.neo4j.graphdb.index.Index
+import org.neo4j.graphdb.Node
+import org.neo4j.graphdb.Relationship
+import org.neo4j.scala.Neo4jWrapper
+import org.neo4j.scala.RestGraphDatabaseServiceProvider
+import org.neo4j.scala.RestTypedTraverser
+import org.neo4j.scala.TypedTraverser
 import models.users.User
+import models.utils.ModelsImplicits.task2node
+import models.utils.ModelsImplicits.user2node
 import models.utils.MyRestGraphDatabaseServiceProvider
+import play.api.Play.current
+import scala.collection.JavaConversions.iterableAsScalaIterable
+import play.api.Logger
 
+case class Task(name: String, folder: String, project: String, done: Boolean, description: Option[String] = None, dueDate: Option[DateTime] = None, owner: Option[String] = None){
+  val creationTime: DateTime = DateTime.now()
+}
 
-case class Task(id: Long, folder: String, project: Long, title: String, done: Boolean, description: Option[String] = None, dueDate: Option[Date] = None, assignedTo: Option[String] = None)
+object Task extends Neo4jWrapper with MyRestGraphDatabaseServiceProvider with RestTypedTraverser with TypedTraverser{
 
-object Task extends Neo4jWrapper with MyRestGraphDatabaseServiceProvider with RestTypedTraverser{
-  
+  lazy val nodeIndex: Index[Node] = getNodeIndex("task").getOrElse{addNodeIndex("task").get}
+  lazy val relationshipIndex: Index[Relationship] = getRelationIndex("task").getOrElse{addRelationshipIndex("task").get}
+
 
   // -- Queries
   
   /**
-   * Retrieve a Task from the id.
+   * Retrieve all tasks.
    */
-  def findById(id: Long): Option[Task] = withTx {
-    implicit neo => 
-    	Neo4jWrapper.toCC[Task](getNodeById(id))
+  def findAll: Seq[Task] = {
+    withTx {
+      implicit neo => {
+        getReferenceNode.doTraverse[Task](follow ->- "TASK") {
+          END_OF_GRAPH
+        } {
+          case (x: Task, _) => true 
+          case _ => false
+        }.toList
+      }
+    }
   }
   
   /**
    * Retrieve tasks for the user.
    */
-  def findInvolving(email: String): Seq[(Task,Project)] = withTx {
+  def findInvolving(email: String): Option[Seq[Task]] = withTx {
     implicit neo => {
-      getNodeById(100)
-      User.findByEmail(email)
-      Seq()
+      User.findByEmail(email).map { user =>
+        val userIndex = getNodeIndex("user").getOrElse{addNodeIndex("user").get}
+        (for (node <- relationshipIndex.get("owner of", user); t = node.getStartNode().toCC[Task].get) yield t) toSeq
+      } orElse {
+        None
+      }
     }
   }
   
@@ -42,13 +66,15 @@ object Task extends Neo4jWrapper with MyRestGraphDatabaseServiceProvider with Re
    * @return a Seq of preceding tasks
    */
   def getPreceding(task: Task): Seq[Task] = withTx {
-    implicit neo => 
-    	Seq[Task]()
+    implicit neo => {
+      (for (node <- relationshipIndex.get("preceding to", task); t = node.getStartNode().toCC[Task].get) yield t) toSeq
+    }    
   }
   
   def isPreceding(dependent: Task, preceding: Task): Boolean = withTx {
-    implicit neo => 
-    	false
+    implicit neo => {
+      getPreceding(dependent) contains(preceding)
+    }
   }
   
   /**
@@ -57,58 +83,133 @@ object Task extends Neo4jWrapper with MyRestGraphDatabaseServiceProvider with Re
    * @param preceding the preceding task
    */
   def addPreceding(dependent: Task, preceding: Task) = withTx {
-    implicit neo =>
+    implicit neo => {
+      val precedingToRel = preceding.createRelationshipTo(dependent, "preceding to")
+      Logger.debug("created new precedeing-to relationship, preceding: " + preceding.getId() + " , dependent: " + dependent.getId())
+      relationshipIndex.add(precedingToRel, "preceding to", dependent)
+      Logger.debug("new precedeing-to relationship added to index")
+
+      val precededByRel = preceding.createRelationshipTo(dependent, "preceded by")
+      Logger.debug("created new preceded-by relationship, preceding: " + preceding.getId() + " , dependent: " + dependent.getId())
+      relationshipIndex.add(precededByRel, "preceded by", preceding)
+      Logger.debug("new preceded-by relationship added to index")
+    }
   }
   
   /**
    * Find tasks related to a project
    */
-  def findByProject(project: Long): Seq[Task] = withTx {
+  def findByNameAndProjectAndFolder(taskName: String, folderName: String, projectName: String): Option[Task] = withTx {    
+    implicit neo => {
+      nodeIndex.get("name+folder+project", taskName + "+" + folderName + "+" + projectName).getSingle().toCC[Task]
+    }
+  }
+
+  /**
+   * Find tasks related to a project
+   */
+  def findByNodeId(id: Long): Option[Task] = withTx {
     implicit neo => 
-    	Seq()
+    	nodeIndex.get("nodeId", id).getSingle().toCC[Task]
   }
 
   /**
    * Delete a task
    */
-  def delete(id: Long) = withTx {
-    implicit neo => 
+  def delete(task: Task) = withTx {
+    implicit neo => {
+      val taskId = task.getId 
+      task.delete()
+      Logger.debug("deleted task id: " + taskId)
+    } 
   }
   
-  /**
-   * Delete all task in a folder.
-   */
-  def deleteInFolder(projectId: Long, folder: String) = withTx {
-    implicit neo => 
-  }
+//  /**
+//   * Delete all task in a folder.
+//   */
+//  def deleteAllInFolder(projectName: String, folder: String) = withTx {
+//    implicit neo => {
+//      
+//    } 
+//  }
   
   /**
    * Mark a task as done or not
    */
-  def markAsDone(taskId: Long, done: Boolean) = withTx {
-    implicit neo => 
+  def markAsDone(task: Task, done: Boolean) = withTx {
+    implicit neo => {
+      task.setProperty("done", done)
+      Logger.debug("task id: " + task.getId() + " done set to " + done)
+    } 
   }
   
-  /**
-   * Rename a folder.
-   */
-  def renameFolder(projectId: Long, folder: String, newName: String) = withTx {
-    implicit neo => 
-  }
-  
-  /**
-   * Check if a user is the owner of this task
-   */
-  def isOwner(task: Long, user: String): Boolean = withTx {
-    implicit neo => 
-    	false
-  }
+  def setOwner (task: Task, email:String) = withTx{
+    implicit neo => {
+      User.findByEmail(email) map { user =>
+        Logger.debug("Setting task owner to " + user.email)
+        task.setProperty("owner", email)
+        val userIndex = getNodeIndex("user").getOrElse{addNodeIndex("user").get}
+        val userRel: Relationship = relationshipIndex.get("owner of", task).getSingle()      
+        Logger.debug("owner relationship acquired, id: " + userRel.getId())
+        relationshipIndex.get("owned by", userRel.getStartNode()).find(_.getStartNode() == task).map { rel =>
+          Logger.debug("owned relationship acquired, id: " + rel.getId())
+          relationshipIndex.remove(rel)
+          Logger.debug("owned relationship removed from index")
+          rel.delete 
+          Logger.debug("owned relationship deleted")
+        } orElse {
+          Logger.debug("owner relationship not found")
+          Some(None)
+        }
+        relationshipIndex.remove(userRel)
+        Logger.debug("owner relationship removed from index")
+        userRel.delete
+        Logger.debug("owner relationship deleted")
+        
+        val newTaskRel: Relationship = task.createRelationshipTo(user, "owned by")
+        Logger.debug("new owned-by relatioship created for task id: " + task.getId() + " and user " + user.email + ", relationship id: " + newTaskRel.getId())
+        relationshipIndex.add(newTaskRel, "owned by", task) 
+        Logger.debug("new owned-by relatioship added to index")
 
+        val newUserRel: Relationship = user.createRelationshipTo(task, "owner of")
+        Logger.debug("new owner-of relatioship created for user " + user.email + " and task id: " + task.getId() + " , relationship id: " + newTaskRel.getId())
+        relationshipIndex.add(newUserRel, "owner of", user) 
+        Logger.debug("new owner-of relatioship added to index")
+      }
+      
+    }
+  }
+  
   /**
    * Create a Task.
    */
-  def create(task: Task): Task = withTx {
+  def create(task: Task): Option[Task] = {
+    findByNameAndProjectAndFolder(task.name, task.folder, task.project) match {
+      case _ => {
+        Logger.debug("task not found")
+        None 
+      }
+    }
+    withTx {
     implicit neo => 
-    	Task(1, "", 1, "", false, None, None, None)
+      val node = createNode(task)
+      Logger.debug("database node create for task id " + node.getId)
+      User.findByEmail(task.owner.get).map { user =>
+        val userIndex = getNodeIndex("user").getOrElse{addNodeIndex("user").get}
+        val nodeRel: Relationship = node.createRelationshipTo(user, "owned by")
+        Logger.debug("new owned-by relatioship created for task id: " + node.getId() + " and user " + user.email + ", relationship id: " + nodeRel.getId())
+        relationshipIndex.add(nodeRel, "owned by", node) 
+        Logger.debug("new owned-by relatioship added to index")
+        val userRel: Relationship = user.createRelationshipTo(node, "owner of")
+        Logger.debug("new owner-of relatioship created for user " + user.email + " and task id: " + task.getId() + " , relationship id: " + userRel.getId())
+        relationshipIndex.add(userRel, "owner of", user)
+        Logger.debug("new owner-of relatioship added to index")
+      }
+	  nodeIndex.add(node, "name+folder+project", task.name + "+" + task.folder + "+" + task.project)
+      Logger.debug("new taks node added to name+folder+project index")
+	  nodeIndex.add(node, "nodeId", node.getId())
+      Logger.debug("new taks node added to nodeId index")
+      Neo4jWrapper.toCC[Task] (node)
+    }      
   }
 }
